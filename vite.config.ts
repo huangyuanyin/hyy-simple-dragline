25954f5d07ad5e83434f64b6e1fc594f0ddd9601
黄原寅 <3443718488@qq.com>
2023-06-01 15:51:04 +0800

    fix tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImports from 'unplugin-auto-import/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImports({
+    AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
         /\.vue$/,
